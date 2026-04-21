11a896a01dc66b590b82acdc3d235c551e59e551
coder149 <75641229+coder149@users.noreply.github.com>
2026-04-20 20:45:26 -0700

    Spelling fix

--- Canvas-Grade-Calculator/frontend/src/App.jsx
+++ Canvas-Grade-Calculator/frontend/src/App.jsx
@@ -1437,7 +1437,7 @@ function App() {
               <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/>
               <line x1="12" y1="17" x2="12.01" y2="17"/>
             </svg>
-            <span>� Showing current Semester 2 grades • Click any course for detailed analyasis and what-if calculations</span>
+            <span>Showing current Semester 2 grades • Click any course for detailed analysis and what-if calculations</span>
           </div>
         </div>
         
